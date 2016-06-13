4e35395fce14515c1eeeacb89174b7802f9dc788
James Briggs <jimy.pbr@gmail.com>
2016-06-13 11:57:36 +0100

    updated the readme and provided further hints in the load test script

--- lmod_load_test.sh
+++ lmod_load_test.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
 unset _MODULESBEGINENV_ _LMFILES_ MODULEPATH LOADEDMODULES MODULESHOME
+# edit this line
 source /slow/data/cosmos/dev/Lmod-6.0.1/install/lmod/lmod/init/profile
 export LMOD_REDIRECT=yes
-export MODULEPATH_ROOT=/slow/data/cosmos/dev/modules_test/modulefiles
+# edit this line to point to your lmod_test/modulefiles directory
+export MODULEPATH_ROOT=/slow/data/cosmos/dev/lmod_test/modulefiles
 export MODULEPATH="$MODULEPATH_ROOT/core:$MODULEPATH"
